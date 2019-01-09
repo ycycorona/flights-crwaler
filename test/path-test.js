1bef48a564d1320175e5f2f9b141ca08eba438e7
yichenyang <yichenyang@mobanker.com>
2019-01-09 18:29:34 +0800

    test文件

--- test/path-test.js
+++ test/path-test.js
@@ -5,3 +5,7 @@ const fs = require('fs-extra')
 fs.ensureDir('./tmp/a', (err) => {
   if (err) console.log(err)
 })
+
+console.log(process.env.NODE_ENV)
+process.env.NODE_ENV = 123
+console.log(process.env.NODE_ENV)
